0fdefc3b91edf9cf38908cbd164d29b4df620713
Shivam Singh <xeroid.space@gmail.com>
2026-03-13 00:21:31 +0530

    Learned prime cycles

--- recursion-backtracking-and-mitm/prime-cycles.cpp
+++ recursion-backtracking-and-mitm/prime-cycles.cpp
@@ -5,39 +5,54 @@ using namespace std;
 
 int N;
 int used[20];
-int start = 1;
+const int start = 1;
 
-bool is_prime(int num) {
-    if (num <= 1) return false;
-    if (num <= 3) return true;
-    if (num % 2 == 0 || num % 3 == 0) return false;
+const int prime_len = 40;
+bool is_prime[prime_len];
+void sieve() {
+    fill(is_prime, is_prime + prime_len, true);
+    is_prime[0] = is_prime[1] = false;
+    
+    for (int num = 2; num <= prime_len / num; num++) {
+        if (!is_prime[num]) continue;
 
-    int fact = 5;
-    while (fact <= num / fact) {
-        if (num % fact == 0) return false;
-        if (num % (fact + 2) == 0) return false;
-        fact += 6;
+        for (int mul = num * num; mul < prime_len; mul += num) {
+            is_prime[mul] = false;
+        }
     }
-
-    return true;
 }
+
+// bool is_prime(int num) {
+//     if (num <= 1) return false;
+//     if (num <= 3) return true;
+//     if (num % 2 == 0 || num % 3 == 0) return false;
+
+//     int fact = 5;
+//     while (fact <= num / fact) {
+//         if (num % fact == 0) return false;
+//         if (num % (fact + 2) == 0) return false;
+//         fact += 6;
+//     }
+
+//     return true;
+// }
 
 bool check(int level, int prev, int ch) {
     if (used[ch]) return false;
 
-    return is_prime(prev + ch);
+    return is_prime[prev + ch];
 }
 
-int rec(int level, int prev) {
+int rec(int level, int prev, int par) {
     if (level == N) {
-        return is_prime(start + prev);
+        return is_prime[start + prev];
     }
 
     int ans = 0;
-    for (int ch = 1; ch <= N; ch++) {
+    for (int ch = 1 + par; ch <= N; ch+=2) {
         if (check(level, prev, ch)) {
             used[ch] = 1;
-            ans += rec(level + 1, ch);
+            ans += rec(level + 1, ch, 1 - par);
             used[ch] = 0;
         }
     }
@@ -45,15 +60,15 @@ int rec(int level, int prev) {
     return ans;
 }
 
-void init() {
+void global_init() {
+    sieve();
     used[start] = 1;
-    cin >> N;
 }
 
 void solve() {
-    init();
+    cin >> N;
     int ans = 0;
-    if (N % 2 == 0) ans = rec(1, start);
+    if (N % 2 == 0) ans = rec(1, start, start % 2 == 1);
 
     cout << ans << endl;
 }
@@ -61,6 +76,8 @@ void solve() {
 signed main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
+
+    global_init();
 
     int t = 1;
     cin >> t;
