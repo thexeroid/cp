8cc49e3025e4400c7714796e16273e86f323ad04
Shivam Singh <xeroid.space@gmail.com>
2026-05-30 22:56:49 +0530

    Digit sequence

---
+++ binary-search/external-problems/digits-sequence.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+const char sp = ' ';
+const char nl = '\n';
+#define print(x) cout << x << nl
+
+void solve() {
+    
+}
+
+signed main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
